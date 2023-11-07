de6242cbd65fd957b2718a669d1d574c023b1332
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-11-07 09:43:19 +0100

    feat(app): Add the necessary module dependencies to the api

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,13 @@ import { AppService } from '@/app.service';
 import { AppController } from '@/app.controller';
 import { DatabaseModule } from '@/database/database.module';
 import { MailjetModule } from '@/mailjet/mailjet.module';
-import { AuthModule } from '@/auth/auth.module';
-import { UsersModule } from '@/users/users.module';
+import { AuthModule } from '@/routes/auth/auth.module';
+import { UsersModule } from '@/routes/users/users.module';
+import { ChannelsModule } from '@/routes/channels/channels.module';
+import { ConversationsModule } from '@/routes/conversations/conversations.module';
+import { FriendsModule } from '@/routes/friends/friends.module';
+import { MessagesModule } from '@/routes/messages/messages.module';
+import { ServersModule } from '@/routes/servers/servers.module';
 
 @Module({
 	imports: [
@@ -15,6 +20,11 @@ import { UsersModule } from '@/users/users.module';
 		MailjetModule,
 		AuthModule,
 		UsersModule,
+		ChannelsModule,
+		ConversationsModule,
+		FriendsModule,
+		MessagesModule,
+		ServersModule,
 	],
 	controllers: [AppController],
 	providers: [AppService],

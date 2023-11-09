69246e8f845120e87b165fd80f6ef17377e20b10
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-11-09 10:46:33 +0100

    fea(pipeline): Create a pipeline for the conversations and the messages when retrieving them throught a get method

---
+++ src/routes/messages/utils/messages.pipeline.ts
@@ -0,0 +1,58 @@
+import { Document, ObjectId } from 'mongodb';
+import { RelationshipType } from '@/routes/relationship/interfaces/relationship.interface';
+
+export const channelMessagesPipeline = (userId: ObjectId, contextId: ObjectId): Document[] => {
+	return [
+		{
+			$match: {
+				contextId: contextId,
+			},
+		},
+		{
+			$lookup: {
+				from: 'users',
+				localField: 'userId',
+				foreignField: '_id',
+				as: 'user',
+			},
+		},
+		{
+			$lookup: {
+				from: 'relationships',
+				let: { userId: '$userId' },
+				pipeline: [
+					{
+						$match: {
+							$expr: {
+								$or: [
+									{ $eq: ['$userIdA', '$$userId'] },
+									{ $eq: ['$userIdB', '$$userId'] },
+								],
+							},
+							type: RelationshipType.BLOCKED,
+						},
+					},
+				],
+				as: 'blockedRelationships',
+			},
+		},
+		{
+			$project: {
+				id: '$_id',
+				isOwner: { $eq: ['$userId', userId] },
+				userPicture: { $arrayElemAt: ['$user.avatarUrl', 0] },
+				username: { $arrayElemAt: ['$user.username', 0] },
+				content: '$content',
+				isEdited: '$isEdited',
+				isBlocked: {
+					$cond: {
+						if: { $gt: [{ $size: '$blockedRelationships' }, 0] },
+						then: true,
+						else: false,
+					},
+				},
+				createdAt: '$createdAt',
+			},
+		},
+	]
+} 

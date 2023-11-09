c489495793ca415a7b1680bab52fa871d0322e93
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-11-09 18:13:33 +0100

    feat(message): Retrieve the conversation for context purpose on each new message

--- src/routes/messages/messages.module.ts
+++ src/routes/messages/messages.module.ts
@@ -6,9 +6,10 @@ import { MessagesController } from '@/routes/messages/messages.controller';
 import { MessagesRepository } from '@/routes/messages/messages.repository';
 import { ConversationsModule } from '@/routes/conversations/conversations.module';
 import { RelationshipsModule } from '@/routes/relationship/relationship.module';
+import { UsersModule } from '@/routes/users/users.module';
 
 @Module({
-	imports: [DatabaseModule.forRoot(), ConversationsModule, RelationshipsModule],
+	imports: [DatabaseModule.forRoot(), ConversationsModule, RelationshipsModule, UsersModule],
 	providers: [MessagesService, MessagesRepository],
 	controllers: [MessagesController],
 })
